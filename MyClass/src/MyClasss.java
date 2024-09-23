ada82ee04421c119aa04be6dbb6b1a461f4f2317
VanLoc25 <dangvanloc256@gmail.com>
2024-09-23 20:08:31 +0700

    Bai1_Helooo

---
+++ MyClass/src/MyClasss.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class MyClasss {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 5;
+		int b = 7;
+		int c = a + b;
+		
+		System.out.println("Heloooooo");
+		System.out.println("Tổng là : "+ c);
+	}
+
+}
