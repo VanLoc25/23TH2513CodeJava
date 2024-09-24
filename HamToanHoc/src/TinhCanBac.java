ba17d0a6332f2ee806a72b7a34f21a20339675e6
VanLoc25 <dangvanloc256@gmail.com>
2024-09-24 19:32:54 +0700

    Căn Bậc

---
+++ HamToanHoc/src/TinhCanBac.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class TinhCanBac {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double a = Math.sqrt(7);
+		
+		 System.out.println("Căn bậc 2 của 7 là: " + a);
+		
+	}
+
+}
