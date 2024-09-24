6da37ac93f64d092cdd46a95b03d33fcdd137991
VanLoc25 <dangvanloc256@gmail.com>
2024-09-24 20:34:19 +0700

    Tinh Bmi

--- TinhChiSoBmi/src/TinhBmi.java
+++ TinhChiSoBmi/src/TinhBmi.java
@@ -36,24 +36,22 @@ public class TinhBmi {
         double bmi = canNang / (chieuCao * chieuCao);
         
         // Xuất kết quả BMI
-        System.out.printf("Chỉ số BMI của bạn là: %.2f%n", + bmi);
+        System.out.println("Bạn " + hoTen);
+		System.out.println("Có chỉ số BMI = " + bmi);
         
        
         
         // Đánh giá theo chỉ số BMI
-
-if (bmi < 18.5) {
-            System.out.println("Thiếu cân.");
-        } 
-	else if (bmi >= 18.5 && bmi < 24.9) {
-            System.out.println("Cân nặng bình thường.");
-        } 
-	else if (bmi >= 25 && bmi < 29.9) {
-            System.out.println("Thừa cân.");
-        } 
-	else {
-            System.out.println("Bị béo phì.");
-        }
+String thongBao;
+if (bmi < 18.5) 
+		thongBao = "Thiếu cân.";
+	else if (bmi >= 18.5 && bmi < 24.9) 
+		thongBao = "Cân nặng bình thường.";
+	else if (bmi >= 25 && bmi < 29.9) 
+		thongBao = "Thừa cân.";
+	else 
+		thongBao = "Bị béo phì.";
+		System.out.println(thongBao);
 	}
 
 }
